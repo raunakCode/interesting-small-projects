7f5c632caf0e214abbba1f8270e04da3755f9c5c
Raunak Redkar <raunak.redkar@gmail.com>
2025-01-13 13:20:41 +0100

    made the bot better

--- othelloBot.cpp
+++ othelloBot.cpp
@@ -7,15 +7,15 @@ using namespace std;
 const int EMPTY = 2;
 const int BLACK = 1;
 const int WHITE = 0;
+const int BOARD_SIZE = 8;
 
 struct GameBoard {
-	//int board[8][8];
 	vector<vector<int>> board;
 	bool turn; //
 	int movesMade;
 
 	GameBoard() {
-		board.assign(8, vector<int>(8, EMPTY));
+		board.assign(BOARD_SIZE, vector<int>(BOARD_SIZE, EMPTY));
 		board[3][3] = board[4][4] = BLACK;
 		board[4][3] = board[3][4] = WHITE;
 
@@ -26,24 +26,24 @@ struct GameBoard {
 	void printBoard() {
 		system("clear");
 		cout << "\033[32m";
-		for(int i = 0; i < 8 * 3 + 1; i++) cout << '-';
+		for(int i = 0; i < BOARD_SIZE * 3 + 1; i++) cout << '-';
 		cout << '\n';
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
+		for(int i = 0; i < BOARD_SIZE; i++) {
+			for(int j = 0; j < BOARD_SIZE; j++) {
 				if (j == 0) cout << '|';
 				if (board[i][j] == EMPTY) cout << "  |";
 				if (board[i][j] == BLACK) cout << "\u26ab|";
 				if (board[i][j] == WHITE) cout << "\u26aa|";
 			}
 			cout << '\n';
-			for(int i = 0; i < 8 * 3 + 1; i++) cout << '-';
+			for(int i = 0; i < BOARD_SIZE * 3 + 1; i++) cout << '-';
 			cout << '\n';
 		}
 		cout << "\033[0m";
 	}
 
 	bool inside(int row, int col) {
-		return (0 <= row && row < 8 && 0 <= col && col < 8);
+		return (0 <= row && row < BOARD_SIZE && 0 <= col && col < BOARD_SIZE);
 	}
 
 	bool gameOver() {
@@ -52,8 +52,8 @@ struct GameBoard {
 	
 	vector<pair<int, int>> getMoves() {
 		vector<pair<int, int>> possibleMoves;
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
+		for(int i = 0; i < BOARD_SIZE; i++) {
+			for(int j = 0; j < BOARD_SIZE; j++) {
 				if (board[i][j] == EMPTY && movePossible(i, j)) {
 					possibleMoves.push_back({i, j});
 				}
@@ -92,12 +92,19 @@ struct GameBoard {
 
 	int evaluate() {
 		int eval = 0;
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				if (board[i][j] == (int) turn) {
-					eval++;	
+		for(int i = 0; i < BOARD_SIZE; i++) {
+			for(int j = 0; j < BOARD_SIZE; j++) {
+				int delta = 0;
+				if (min(i, j) == 0 || max(i, j) == BOARD_SIZE - 1) {
+					if ((min(i, j) == 1 && max(i, j) == BOARD_SIZE) || (i == j)) {
+						delta = 100;
+					}
+					else delta = 50;
 				}
-				else if (board[i][j] == (((int)turn) ^ 1)) eval--;
+				if (board[i][j] == (int) turn) {
+					eval += delta;
+				}
+				else if (board[i][j] == (((int)turn) ^ 1)) eval -= delta;
 			}
 		}
 		return eval;
@@ -106,6 +113,7 @@ struct GameBoard {
 	int search(int depth) {
 		if (depth == 0) return evaluate();
 		int minEval = 100000000;
+		//int minEval = -100000000;
 		vector<pair<int, int>> moves = getMoves();	
 		for(auto &mv: moves) {
 			GameBoard curBoard;
@@ -113,9 +121,10 @@ struct GameBoard {
 			curBoard.turn = turn;
 			curBoard.movesMade = movesMade;
 			curBoard.tryMove(mv.first, mv.second);
+			//minEval = min(curBoard.search(depth - 1), minEval);
 			minEval = min(curBoard.search(depth - 1), minEval);
 		}
-		return minEval;
+		return -minEval;
 	}
 
 	void performBestMove(int depth) {
@@ -128,12 +137,11 @@ struct GameBoard {
 			curBoard.turn = turn;
 			curBoard.movesMade = movesMade;
 			curBoard.tryMove(mv.first, mv.second);
-			int curEval = curBoard.search(2);
+			int curEval = -curBoard.search(depth);
 			if (curEval > maxEval) {
 				maxEval = curEval;
 				bestMove = mv;
 			}
-			maxEval = max(curBoard.search(depth), maxEval);
 		}
 		tryMove(bestMove.first, bestMove.second);
 	}
@@ -197,14 +205,18 @@ int main() {
 		}
 		else {
 			board.printBoard();
+			sleep(0.5);
 			board.performBestMove(6);
 			sleep(2);
 			board.printBoard();
 			if (board.gameOver()) {
-				if (board.evaluate() > 0) {
-					cout << "Black wins";
+				int white = 0, black = 0;
+				for(int i = 0; i < BOARD_SIZE; i++) {
+					white += count(board.board[i].begin(), board.board[i].end(), 0);
+					black += count(board.board[i].begin(), board.board[i].end(), 1);
 				}
-				else cout << "White wins";
+				if (white == black) cout << "Tie.";
+				else cout << (white > black ? "White " : "Black ") << "wins.";
 				break;
 			}
 		}
